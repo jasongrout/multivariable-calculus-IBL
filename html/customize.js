cff1f0779de2c2320c14aa6163a9bb7a374aed25
Jason Grout <jason.grout@drake.edu>
2013-09-17 08:46:32 -0500

    Convert things to use a small bit of boilerplate at the top of the script.

--- html/customize.js
+++ html/customize.js
@@ -6,5 +6,13 @@ button.click(function() {
     body.toggleClass('fullwidth');
     $('.marginpar').toggle();
 })
-body.prepend(button);
+    body.prepend(button);
+    body.prepend('<a href="index.html">Return to Table of Contents</a><br/>');
+
+    $("body a[href^='http://']").attr("target","_blank");
+    $("body a[href^='https://']").attr("target","_blank");
+    sagecell.makeSagecell({"inputLocation": ".sage"});
+    sagecell.makeSagecell({"inputLocation": ".sageminimal", template:
+sagecell.templates.minimal})
+
 })
