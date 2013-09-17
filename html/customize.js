7e3e58835da6d81eeb6a2ee9108f1f49e4280bcc
Jason Grout <jason.grout@drake.edu>
2013-09-17 09:19:51 -0500

    Add table of contents to each chapter

--- html/customize.js
+++ html/customize.js
@@ -6,13 +6,22 @@ button.click(function() {
     body.toggleClass('fullwidth');
     $('.marginpar').toggle();
 })
-    body.prepend(button);
-    body.prepend('<a href="index.html">Return to Table of Contents</a><br/>');
-
     $("body a[href^='http://']").attr("target","_blank");
     $("body a[href^='https://']").attr("target","_blank");
     sagecell.makeSagecell({"inputLocation": ".sage"});
     sagecell.makeSagecell({"inputLocation": ".sageminimal", template:
 sagecell.templates.minimal})
 
+    var toc=$('<ol></ol>')
+    $('body section section:visible').each(function(i,e) {
+        var sect = $(e)
+        var title = sect.children('h1')
+        var id = 'section-'+i;
+        sect.attr('id', id);
+        
+        toc.append('<li><a href=#'+id+'>'+title.html()+'</a></li>')
+    })
+    body.prepend(toc);
+    body.prepend(button);
+    body.prepend('<a href="index.html">Return to Table of Contents</a><br/>');
 })
